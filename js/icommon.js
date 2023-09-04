24915cb3e106ac9dc0b21fcc97d8522dfd4dde40
yunajeon96 <139088289+yunajeon96@users.noreply.github.com>
2023-09-04 15:26:02 +0900

    2023.09.04(final)

--- js/icommon.js
+++ js/icommon.js
@@ -1,12 +1,18 @@
 //menu hover p 나타남.
-let fadehs = document.querySelectorAll('aside a h1');
+if(window.innerWidth >= 430){
+    let fadehs = document.querySelectorAll('aside a h1');
 
-fadehs.forEach(function(fadeh){
-    fadeh.onmouseover = function(){
-        this.nextSibling.style.opacity = 1
-    }
+    fadehs.forEach(function(fadeh){
+        fadeh.onmouseover = function(){
+            this.nextSibling.style.opacity = 1
+        }
 
-    fadeh.onmouseout = function(){
-        this.nextSibling.style.opacity = 0
-    }
-})
+        fadeh.onmouseout = function(){
+            this.nextSibling.style.opacity = 0
+        }
+    })
+}
+
+$(".icon").click(function(){
+    $("aside a:not(:first-of-type)").stop().slideToggle(1500);
+});
